996e30fa1104e092d004f2f19ade12946f26072d
diarmuidmcgonagle <diarmuidmcgonagle@gmail.com>
2024-07-25 11:42:01 +0200

    set supplieykey and user solution

--- MealzOnboardingProviderUIKit/Mealz/MealzManager.swift
+++ MealzOnboardingProviderUIKit/Mealz/MealzManager.swift
@@ -15,7 +15,23 @@ public class MealzManager: ObservableObject {
     // TODO: 3. Set SupplierKey, Store & User Id
 
     // TODO 3a. Copy & paste supplierID into variable
+    let providerKey = "ewogICAgICAgICJwcm92aWRlcl9pZCI6ICJtYXJtaXRvbiIKCSJwbGF1c2libGVfZG9tYWluZSI6ICJtaWFtLm1hcm1pdG9uLmFwcCIsCgkibWlhbV9vcmlnaW4iOiAibWFybWl0b24iLAoJIm9yaWdpbiI6ICJtaWFtLm1hcm1pdG9uLmFwcCIsCgkibWlhbV9lbnZpcm9ubWVudCI6ICJVQVQiCn0="
     // TODO 3b. Init Mealz
-    // TODO 3c. Allow authless users
+    private init() {
+        Mealz.shared.Core(init: { coreBuilder in
+            // set supplier key
+            coreBuilder.sdkRequirement(init: { [self] requirementBuilder in
+                requirementBuilder.key = providerKey
+            })
+            // TODO 3c. Allow authless users
+            coreBuilder.option(init: { config in
+                config.isAnonymousModeEnabled = true
+            })
+        })
+    }
+   
     // TODO 3d. Create function to set the user
+    func updateUserId(userId: String?) {
+        Mealz.shared.user.updateUserId(userId: userId, authorization: Authorization.userId)
+    }
 }
